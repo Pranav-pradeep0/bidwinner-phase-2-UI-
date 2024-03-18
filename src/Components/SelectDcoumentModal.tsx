3fdb97380fc4f92ee000184aa2f22332b97f4d95
Pranav-pradeep0 <pranavpradeep1120@gmail.com>
2024-03-18 18:40:35 +0530

    accordian open close fix

--- src/Components/SelectDcoumentModal.tsx
+++ src/Components/SelectDcoumentModal.tsx
@@ -1,9 +1,9 @@
-import { Box, Modal } from "@mui/material";
+import { Box, Modal, Typography } from "@mui/material";
 import React, { useState } from "react";
 import DocumentSerchBar from "./SelectDocumentComponents/DocumentSerchBar";
 import DocumentAccordians from "./SelectDocumentComponents/DocumentAccordians";
 import { X } from "@phosphor-icons/react";
-import DrawerImage from '../assets/SECURITY.png'
+// import DrawerImage from "../assets/SECURITY.png";
 import ButtonGradient from "./reusableComponents/Button";
 
 const style = {
@@ -16,7 +16,7 @@ const style = {
   boxShadow: 24,
   p: 4,
   display: "flex",
-  gap: "10px"
+  gap: "10px",
 };
 
 interface DocumentModalProps {
@@ -28,12 +28,11 @@ const SelectDcoumentModal: React.FC<DocumentModalProps> = ({
   open,
   setOpen,
 }) => {
-
-  const [imgURL, setImgURL] = useState("")
+  const [imgURL, setImgURL] = useState("");
   // const [open, setOpen] = useState(false);
   // const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-  console.log("imgURL", imgURL);
+
 
   return (
     <Box>
@@ -48,19 +47,62 @@ const SelectDcoumentModal: React.FC<DocumentModalProps> = ({
             <DocumentSerchBar />
             <DocumentAccordians setImgURL={setImgURL} />
           </Box>
-          <Box sx={{ flex: 1, display: "flex", justifyContent: "center", alignItems: "center" }}>
+          <Box
+            sx={{
+              flex: 1,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
             <Box sx={{ width: "100%", height: "70%" }}>
-              <img src={imgURL} alt="" style={{ width: "100%", height: "100%", objectFit: "cover" }} />
+              {imgURL ? (
+                <img
+                  src={imgURL}
+                  alt=""
+                  style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                />
+              ) : (
+                <Typography
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    height: "100%",
+                  }}
+                >
+                  Select a image to preview
+                </Typography>
+              )}
             </Box>
           </Box>
-          <Box sx={{ position: "absolute", top: "20px", right: "20px", cursor: "pointer", fontSize: "25px", color: "gray" }} onClick={() => setOpen(false)} >
+          <Box
+            sx={{
+              position: "absolute",
+              top: "20px",
+              right: "20px",
+              cursor: "pointer",
+              fontSize: "25px",
+              color: "gray",
+            }}
+            onClick={() => setOpen(false)}
+          >
             <X />
           </Box>
-          <Box sx={{ position: "absolute", bottom: "40px", right: "40px", cursor: "pointer" }}>
-            <ButtonGradient handleClick={() => console.log("hello")} title="Rename" />
+          <Box
+            sx={{
+              position: "absolute",
+              bottom: "40px",
+              right: "40px",
+              cursor: "pointer",
+            }}
+          >
+            <ButtonGradient
+              handleClick={() => console.log("hello")}
+              title="Rename"
+            />
           </Box>
         </Box>
-
       </Modal>
     </Box>
   );
--- src/Components/SelectDocumentComponents/DocumentAccordians.tsx
+++ src/Components/SelectDocumentComponents/DocumentAccordians.tsx
@@ -6,46 +6,65 @@ import {
   Typography,
 } from "@mui/material";
 import { CaretDown, FolderNotch } from "@phosphor-icons/react";
-
-import Drawing1 from "../../assets/SECURITY.png"
-import Drawing2 from "../../assets/SECURITY2.png"
-import Drawing3 from "../../assets/FLOOR.png"
 import { useState } from "react";
-// import { useState } from "react";
+
+import Drawing1 from "../../assets/SECURITY.png";
+import Drawing2 from "../../assets/SECURITY2.png";
+import Drawing3 from "../../assets/FLOOR.png";
 
 interface dataProps {
   title: string;
-  items: any
+  items: any;
 }
 
 const data: dataProps[] = [
   {
     title: "Al Hasan Building Drawing Electrical Estimation Document.pdf",
-    items: [{ title: "Page No 1", url: Drawing1 }, { title: "Page No 2", url: Drawing2 }, { title: "Page No 3", url: Drawing3 }],
+    items: [
+      { title: "Page No 1", url: Drawing1 },
+      { title: "Page No 2", url: Drawing2 },
+      { title: "Page No 3", url: Drawing3 },
+    ],
   },
   {
     title: "Al Jaseera Appartment Drawing Electrical Estimation Document.pdf",
-    items: [{ title: "Page No 1", url: Drawing1 }, { title: "Page No 2", url: Drawing2 }, { title: "Page No 3", url: Drawing3 }],
+    items: [
+      { title: "Page No 1", url: Drawing1 },
+      { title: "Page No 2", url: Drawing2 },
+      { title: "Page No 3", url: Drawing3 },
+    ],
   },
 ];
 
-// const [expanded,setExpanded]=useState(false)
 interface DocumentModalProps {
-  setImgURL: any
+  setImgURL: any;
 }
+
 const DocumentAccordians: React.FC<DocumentModalProps> = ({ setImgURL }) => {
-  const [title, setTitle] = useState("")
+  const [expanded, setExpanded] = useState<string | false>(false);
+  const [title, setTitle] = useState("");
+
+  const handleChange = (panel: string) => (
+    event: React.SyntheticEvent,
+    isExpanded: boolean
+  ) => {
+    event
+    setExpanded(isExpanded ? panel : false);
+    setTitle(""); // Reset title when accordion is closed
+  };
 
   const handleClickTitle = (title: string, url: string) => {
-    setImgURL(url)
-    setTitle(title)
-  }
+    setImgURL(url);
+    setTitle(title);
+  };
+
   return (
     <Box>
-      {data?.map((data, ind) => (
+      {data.map((data, ind) => (
         <Accordion
-          // expanded={expanded === 'panel1'}
           key={ind}
+          expanded={expanded === `panel${ind}`}
+          onChange={handleChange(`panel${ind}`)}
           sx={{
             marginTop: "20px",
             boxShadow: "none",
@@ -59,8 +78,8 @@ const DocumentAccordians: React.FC<DocumentModalProps> = ({ setImgURL }) => {
               borderBottom: "rgba(0, 0, 0, 0.3) 1px solid",
             }}
             expandIcon={<CaretDown size={26} />}
-            aria-controls="panel1-content"
-            id="panel1-header"
+            aria-controls={`panel${ind}-content`}
+            id={`panel${ind}-header`}
           >
             <FolderNotch size={22} />
             <Typography sx={{ fontWeight: 500, marginInline: "10px" }}>
@@ -68,10 +87,20 @@ const DocumentAccordians: React.FC<DocumentModalProps> = ({ setImgURL }) => {
             </Typography>
           </AccordionSummary>
           <AccordionDetails>
-            {data?.items?.map((items: any, ind: number) => (
-              <Box key={ind}
-              sx={{ padding: "10px 5px", marginBottom: "5px" , cursor: "pointer" , backgroundColor: title === items.title ? "#f2f6f9" : "", ':hover': { backgroundColor: "#f2f6f9" } }}
-                onClick={() => handleClickTitle(items.title, items.url)} >
+            {data.items.map((items: any, ind: number) => (
+              <Box
+                key={ind}
+                sx={{
+                  padding: "10px 5px",
+                  marginBottom: "5px",
+                  cursor: "pointer",
+                  borderRadius: "10px",
+                  paddingInline: "20px",
+                  backgroundColor: title === items.title ? "#E3F3FF" : "",
+                  ":hover": { backgroundColor: "#E3F3FF" },
+                }}
+                onClick={() => handleClickTitle(items.title, items.url)}
+              >
                 {items.title}
               </Box>
             ))}
