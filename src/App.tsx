5ab2678f3f44385fa7ce1093da5dfb00edfd7c78
Pranav-pradeep0 <pranavpradeep1120@gmail.com>
2024-03-20 10:14:51 +0530

    minor fixes and changes

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
       {/* <SelectDcoumentModal/> */}
       {/* <DisplayPage /> */}
       <TakeoffMainPage />
-      {/* <Toolbar/> */}
+      {/* <Toolbar /> */}
     </>
   );
 }
--- src/Components/Takeoff/TakeoffMainPage.tsx
+++ src/Components/Takeoff/TakeoffMainPage.tsx
@@ -1,7 +1,12 @@
 import { ReactNode, useState } from "react";
 import Drawing from "../../assets/SECURITY.png";
 import { Box, Button, MenuItem, Select } from "@mui/material";
-import { CaretLeft, CaretRight, FileArrowDown, ShareNetwork } from "@phosphor-icons/react";
+import {
+  CaretLeft,
+  CaretRight,
+  FileArrowDown,
+  ShareNetwork,
+} from "@phosphor-icons/react";
 import ImageZoomcomponent from "../ImageZoomcomponent";
 import Toolbar from "../Toolbar";
 
@@ -33,37 +38,38 @@ const styles = {
 };
 
 const GrayBox = ({ children }: { children: ReactNode }) => {
-  return <div style={styles.arrowBox}>
-    {children}
-  </div>
-}
+  return <div style={styles.arrowBox}>{children}</div>;
+};
 
 const TakeoffMainPage = () => {
-
   const [toolMethods, setToolMethod] = useState({
     pan: false,
     select: false,
-    dot: false
-  })
+    dot: false,
+  });
 
   const [rectCoordinates, setRectCoordinates] = useState({});
-  // console.log(rectCoordinates);
+  console.log(rectCoordinates);
 
   return (
-    <div style={styles.container}>
+    <Box sx={styles.container}>
       <Box sx={styles.toolNavbar}>
-        <div style={{ display: "flex", gap: "15px", }}>
+        <Box sx={{ display: "flex", gap: "15px" }}>
           <GrayBox>
             <CaretLeft size={23} />
           </GrayBox>
-          <Select displayEmpty inputProps={{ "aria-label": "Without label" }} sx={{
-            '& .css-11u53oe-MuiSelect-select-MuiInputBase-input-MuiOutlinedInput-input': {
-              padding: '6px 10px !important',
-              width: "400px",
-              height: "26px",
-
-            },
-          }} >
+          <Select
+            displayEmpty
+            inputProps={{ "aria-label": "Without label" }}
+            sx={{
+              "& .css-11u53oe-MuiSelect-select-MuiInputBase-input-MuiOutlinedInput-input":
+                {
+                  padding: "6px 10px !important",
+                  width: "400px",
+                  height: "26px",
+                },
+            }}
+          >
             <MenuItem value={10}>First floor guest room drawing.pdf</MenuItem>
             <MenuItem value={20}>Fourth floor guest room drawing.pdf</MenuItem>
             <MenuItem value={30}>Fifth floor guest room drawing.pdf</MenuItem>
@@ -71,43 +77,56 @@ const TakeoffMainPage = () => {
           <GrayBox>
             <CaretRight size={23} />
           </GrayBox>
-          <Button color="primary" variant="contained" sx={{
-            '&.MuiButton-contained': {
-              boxShadow: 'none !important',
-              borderRadius: "5px"
-            }
-          }} >
+          <Button
+            color="primary"
+            variant="contained"
+            sx={{
+              "&.MuiButton-contained": {
+                boxShadow: "none !important",
+                borderRadius: "5px",
+              },
+              backgroundColor: "#3153CD",
+              ":hover": {
+                backgroundColor: "#3153CD",
+              },
+            }}
+          >
             Compare
           </Button>
-          <GrayBox >
+          <GrayBox>
             <ShareNetwork size={23} />
           </GrayBox>
           <GrayBox>
             <FileArrowDown size={23} />
           </GrayBox>
-        </div>
-        <div>
-          <Select displayEmpty inputProps={{ "aria-label": "Without label" }} sx={{
-            '& .css-11u53oe-MuiSelect-select-MuiInputBase-input-MuiOutlinedInput-input': {
-              padding: '6px 10px !important',
-              width: "250px",
-              height: "26px",
-              color: "#3153CD"
-            },
-          }} >
+        </Box>
+        <Box>
+          <Select
+            displayEmpty
+            inputProps={{ "aria-label": "Without label" }}
+            sx={{
+              "& .css-11u53oe-MuiSelect-select-MuiInputBase-input-MuiOutlinedInput-input":
+                {
+                  padding: "6px 10px !important",
+                  width: "250px",
+                  height: "26px",
+                  color: "#3153CD",
+                },
+            }}
+          >
             <MenuItem value={10}>Drawing Scale 1:80</MenuItem>
             <MenuItem value={20}>Drawing Scale 1:100</MenuItem>
             <MenuItem value={30}>Drawing Scale 1:200</MenuItem>
           </Select>
-        </div>
-      </Box >
+        </Box>
+      </Box>
       <ImageZoomcomponent
         src={Drawing}
         setRectCoordinates={setRectCoordinates}
         toolMethods={toolMethods}
       />
       <Toolbar setToolMethod={setToolMethod} />
-    </div >
+    </Box>
   );
 };
 
